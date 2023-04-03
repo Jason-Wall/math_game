f305c9dcc47a6a186bbc49107cd7790131db101c
Jason-Wall <jason.waldick@gmail.com>
2023-04-03 10:23:04 -0600

    create Game class with self method for starting game

---
+++ Game.rb
@@ -0,0 +1,9 @@
+# Game class - Contains and runs game logic, accepts a list of players.
+
+class Game
+
+  def self.start_game
+    puts 'the game has started'
+  end
+end
+
--- main.rb
+++ main.rb
@@ -2,12 +2,14 @@
 
 # Require in classes:
 require './Player'
+require './Game'
 
 # Main Thread:
-
 
 bill = Player.new('Bill')
 
 puts bill.name
 puts bill.lives
+
+Game.start_game
 
